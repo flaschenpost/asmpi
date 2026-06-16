01e7d126ae2d7e18a568e39760a5869cecc8f925
marco <mg@cantamen.de>
2026-06-16 23:26:54 +0200

    working in c

--- t1.c
+++ t1.c
@@ -7,6 +7,12 @@ const uint16_t divi1 = 239*239;
 int main() {
     uint64_t a; // High 64 bits
     uint64_t b; // Low 64 bits
+    uint64_t c; // Low 64 bits
+    
+    uint64_t mq;
+    uint64_t mr;
+    uint64_t q; // Low 64 bits
+    uint64_t r; // Low 64 bits
 
     uint64_t rdx; // High 64 bits
     uint64_t rax; // Low 64 bits
@@ -21,26 +27,32 @@ int main() {
     printf("0x%016lx %08x\n", inv, divi1);
     
     uint64_t max = 1;
-    max <<= 49;
+    max <<= 63;
     max--;
 
+    uint64_t mask = (1ull<<16) - 1;
     uint64_t min = 0x7ea0000000;
     //uint64_t min = 1;
-    for (uint64_t i=min; i<max; i+=0xf7a){
+    for (uint64_t i=min; i<max; i+=0x1e5){
       prod = i;
       prod *= inv;
 
-      a = (uint64_t) (prod>>64);
+      q = i/divi1;
+      r = i % divi1;
+
       b = (uint64_t) (prod>>52) +1;
-      //printf("m. %lx %016lx %016lx\n", i, a, b);
+      a = b >> 16;
+      c = ((b & mask)*divi1)>>16;
+      mq = a;
+      mr = c;
+
+      //printf("m. i= %lx a= %016lx b= %016lx c = %016lx q = %016lx r = %016lx \n", i, a, b, c, i/divi1, i%divi1);
 
       prod = b;
       prod *= divi1;
-      //prod += divi1;
       a = (uint64_t)(prod>>16);
-      b = (uint64_t)prod;
-      //printf("r. %lx %016lx %016lx %lu\n", i, a,b, 0ul);
-      if (a == i){
+      //printf("r. %lx %016lx \n", i, a);
+      if (q == mq && r == mr){
         continue;
       }
       printf("3. %lx %016lx %016lx \n--\n\n",i, a,b);
